63d361f7865c7385d2ce6596287688cb87451a70
evsasse <evsasse@gmail.com>
2017-05-30 21:12:24 -0700

    when joining a finishing thread, return

--- src/abstraction/thread.cc
+++ src/abstraction/thread.cc
@@ -80,8 +80,10 @@ int Thread::join()
 
     db<Thread>(TRC) << "Thread::join(this=" << this << ",state=" << _state << ")" << endl;
 
-    _joinedBy.insert(&Thread::running()->_link);
-	Thread::running()->wait();
+    if(_state != FINISHING){
+        _joinedBy.insert(&Thread::running()->_link);
+        Thread::running()->wait();
+    }
 
     unlock();
 
